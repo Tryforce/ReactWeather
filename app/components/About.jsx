8a1c7d6428551777c700ff32372d5a985a43b71a
Kyle Baker <kyle.baker@mrs-electronic.com>
2017-04-06 11:55:54 -0400

    Initial commit

---
+++ app/components/About.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var About = (props) => <h3>About Component</h3>;
+
+// var About = (props) => {
+//     return (
+//         <h3>About Component</h3>
+//     )
+// };
+
+module.exports = About;
